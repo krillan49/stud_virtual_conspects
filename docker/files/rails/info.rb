9e142e87916fe87a1b51a169e51de4062ead5391
krillan <pushkinidze49@gmail.com>
2025-02-24 02:33:13 +0300

    fgdgdrdsetrstr

--- docker/files/rails/info.rb
+++ docker/files/rails/info.rb
@@ -1,5 +1,7 @@
 puts "                                          Проект на Rails 8"
 
+# ( !!! пройти  https://gist.github.com/nav-mike/660712caebe6f6173e067781427d9025)
+
 # 1. Создайте новый Rails проект (если еще не создан):
 # $ rails new myapp --database=postgresql
 # $ cd myapp
@@ -11,12 +13,20 @@ puts "                                          Проект на Rails 8"
 # 4. Необходимо установить Node.js, который включен в `Dockerfile`
 
 # 5. Соберите и запустите контейнеры (приложение):
-# $ docker-compose up --build
+# $ docker compose up --build
 
 # 6. Создайте базу данных (?? Постгре ??):
-# $ docker-compose exec web rails db:create
+# $ docker compose exec web rails db:create
 # Либо выполнить миграции базы данных
-# $ docker-compose run web rails db:migrate
+# $ docker compose run web rails db:migrate
 # web - имя сервиса в docker-compose.yml
 
 # 7. Открыть приложени по адресу http://localhost:3000
+
+# 8. Команды, которые могут использоваться для работы с rails-проектом, в котором используется docker. В качестве примера используется контейнер(сервис ??) web
+# $ docker compose run web rake db:drop	           - Удалить базу данных проекта
+# $ docker compose run web rake db:create	         - Создать базу данных проекта
+# $ docker compose run web rake db:migrate	       - Запустить миграции
+# $ docker compose run web rake db:seed	           - Заполнение базы данных сидами
+# $ docker compose run web bundle	                 - Установка gem-ов. Эта команда добавит gem в Gemfile.lock. После этой команды нужно выполнить сборку контейнера
+# $ docker compose run web rspec spec	             - Запуск тестов rspec
